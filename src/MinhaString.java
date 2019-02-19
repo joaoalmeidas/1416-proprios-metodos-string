8176953d9034c891f260b01da0190779b966a5b5
joao <joao182cx@gmail.com>
2019-02-19 14:53:02 -0300

    Criação dos métodos indexOf() e lastIndexOf() na classe MinhaString.

--- src/MinhaString.java
+++ src/MinhaString.java
@@ -1,4 +1,39 @@
 
-public class MinhaString implements String {
+public class MinhaString {
+	
+	private String string;
 
+	public MinhaString(String string) {
+		this.string = string;
+	}
+	
+	public int indexOf(char c) {
+		
+		for(int i = 0; i < string.length(); i++) {
+			
+			if(string.charAt(i) == c) {
+				
+				return i;
+				
+			}
+			
+		}
+		
+		return -1;
+	}
+	
+	public int lastIndexOf(char c) {
+		
+		for(int i = string.length() - 1; i >= 0; i--) {
+			
+			if(string.charAt(i) == c) {
+				
+				return i;
+				
+			}
+			
+		}
+		
+		return -1;
+	}
 }
